b1adc213527dcde26e1dc68e7fd2556a9fcf16a3
Hans Lichtner <112954637+HansLichtner@users.noreply.github.com>
2025-09-23 19:11:57 -0300

    Update io.py

--- common/io.py
+++ common/io.py
@@ -8,20 +8,19 @@ from numpy import byte, int16, int32, int64, ubyte, uint16, uint32, uint64
 
 offs_t = NewType('offs_t', int)
 
-
 #=====================================================================
 
-class Endian(enum.StrEnum):
+# Correção simplificada para Endian
+class Endian:
     LITTLE = '<'
     BIG = '>'
-
 
 #=====================================================================
 #   Write
 #=====================================================================
 #region
 # String
-def WriteString(f: BufferedWriter, s: str, endian: Endian = Endian.LITTLE) -> None:
+def WriteString(f: BufferedWriter, s: str, endian = Endian.LITTLE) -> None:
     if type(s) is bytes:
         f.write(s)
     else:
@@ -29,11 +28,11 @@ def WriteString(f: BufferedWriter, s: str, endian: Endian = Endian.LITTLE) -> No
 
 
 # Byte
-def WriteUByte(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:      
+def WriteUByte(f: BufferedWriter, v, endian = Endian.LITTLE) -> None:      
     # f.write( pack( endian + 'B', int(v) ) )
     f.write( pack( 'B', int(v) ) )
 
-def WriteSByte(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:  #signed    
+def WriteSByte(f: BufferedWriter, v, endian = Endian.LITTLE) -> None:  #signed    
     # f.write(pack ( endian + 'b', int(v) ) )
     f.write( pack ( 'b', int(v) ) )
 
@@ -43,35 +42,34 @@ def WriteBytes(f: BufferedWriter, v, count) -> None:
 
 
 # Short
-def WriteUInt16(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:      
-    f.write( pack( endian + 'H', int(v) ) )
+def WriteUInt16(f: BufferedWriter, v, endian = Endian.LITTLE) -> None:      
+    f.write( pack( str(endian) + 'H', int(v) ) )
 
-def WriteSInt16(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:  #signed
-    f.write( pack( endian + 'h', int(v) ) )
+def WriteSInt16(f: BufferedWriter, v, endian = Endian.LITTLE) -> None:  #signed
+    f.write( pack( str(endian) + 'h', int(v) ) )
 
 
 # Int
-def WriteUInt32(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:      
-    f.write( pack( endian + 'L', v) )
+def WriteUInt32(f: BufferedWriter, v, endian = Endian.LITTLE) -> None:      
+    f.write( pack( str(endian) + 'L', v) )
 
-def WriteSInt32(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:  #signed    
-    f.write( pack( endian + 'l', v) )
+def WriteSInt32(f: BufferedWriter, v, endian = Endian.LITTLE) -> None:  #signed    
+    f.write( pack( str(endian) + 'l', v) )
 
 
 # Int64
-def WriteUInt64(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:      
-    f.write( pack( endian + 'Q', v) )
+def WriteUInt64(f: BufferedWriter, v, endian = Endian.LITTLE) -> None:      
+    f.write( pack( str(endian) + 'Q', v) )
 
-def WriteSInt64(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:  #signed
-    f.write( pack( endian + 'q', v) )
+def WriteSInt64(f: BufferedWriter, v, endian = Endian.LITTLE) -> None:  #signed
+    f.write( pack( str(endian) + 'q', v) )
 
 
 # Float
-def WriteFloat(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:
-    f.write( pack( endian + 'f', v) )
+def WriteFloat(f: BufferedWriter, v, endian = Endian.LITTLE) -> None:
+    f.write( pack( str(endian) + 'f', v) )
 
 #endregion    
-
 
 #=====================================================================
 #   Read
@@ -79,64 +77,64 @@ def WriteFloat(f: BufferedWriter, v, endian: Endian = Endian.LITTLE) -> None:
 #region
 # String
 def ReadString(f: BufferedReader, size: int = 0, encoding: str = "utf-8") -> str:
-		if size == 0:
-			ret = ReadCString(f)
-		else:
-			ret = unpack( "<" + "%is" % (size), f.read(size) )[0]
+    if size == 0:
+        ret = ReadCString(f)
+    else:
+        ret = unpack( "<" + "%is" % (size), f.read(size) )[0]
 
-		return ret.decode(encoding)
+    return ret.decode(encoding)
 
 def ReadCString(f: BufferedReader) -> bytes:
-		ret = []
-		c = b""
+    ret = []
+    c = b""
 
-		while c != b"\0":
-			ret.append(c)
-			c = f.read(1)
+    while c != b"\0":
+        ret.append(c)
+        c = f.read(1)
 
-			if not c:
-				raise ValueError("Unterminated string: %r" % (ret))
+        if not c:
+            raise ValueError("Unterminated string: %r" % (ret))
 
-		return b"".join(ret)
+    return b"".join(ret)
 
 
 # Byte
-def ReadUByte(f: BufferedReader, endian: Endian = Endian.LITTLE) -> ubyte:   
+def ReadUByte(f: BufferedReader, endian = Endian.LITTLE) -> ubyte:   
     # return unpack( endian + 'B', f.read(1) )[0]
     return unpack( 'B', f.read(1) )[0]
 
-def ReadByte(f: BufferedReader, endian: Endian = Endian.LITTLE) -> byte:  #signed
+def ReadByte(f: BufferedReader, endian = Endian.LITTLE) -> byte:  #signed
     # return unpack( endian + 'b', f.read(1) )[0]
     return unpack( 'b', f.read(1) )[0]
 
 
 # Short
-def ReadUInt16(f: BufferedReader, endian: Endian = Endian.LITTLE) -> uint16:
-    return unpack( endian + 'H', f.read(2) )[0]
+def ReadUInt16(f: BufferedReader, endian = Endian.LITTLE) -> uint16:
+    return unpack( str(endian) + 'H', f.read(2) )[0]
 
-def ReadSInt16(f: BufferedReader, endian: Endian = Endian.LITTLE) -> int16: #signed
-    return unpack( endian + 'h', f.read(2) )[0]
+def ReadSInt16(f: BufferedReader, endian = Endian.LITTLE) -> int16: #signed
+    return unpack( str(endian) + 'h', f.read(2) )[0]
 
 
 # Int
-def ReadUInt32(f: BufferedReader, endian: Endian = Endian.LITTLE) -> uint32:
-    return unpack( endian + 'L', f.read(4) )[0]
+def ReadUInt32(f: BufferedReader, endian = Endian.LITTLE) -> uint32:
+    return unpack( str(endian) + 'L', f.read(4) )[0]
     
-def ReadSInt32(f: BufferedReader, endian: Endian = Endian.LITTLE) -> int32:  #signed
-    return unpack( endian + 'l', f.read(4) )[0]
+def ReadSInt32(f: BufferedReader, endian = Endian.LITTLE) -> int32:  #signed
+    return unpack( str(endian) + 'l', f.read(4) )[0]
 
 
 # Int64
-def ReadUInt64(f: BufferedReader, endian: Endian = Endian.LITTLE) -> uint64:  
-    return unpack( endian + 'Q', f.read(8) )[0]
+def ReadUInt64(f: BufferedReader, endian = Endian.LITTLE) -> uint64:  
+    return unpack( str(endian) + 'Q', f.read(8) )[0]
 
-def ReadSInt64(f: BufferedReader, endian: Endian = Endian.LITTLE) -> int64:  #signed
-    return unpack( endian + 'q', f.read(8) )[0]
+def ReadSInt64(f: BufferedReader, endian = Endian.LITTLE) -> int64:  #signed
+    return unpack( str(endian) + 'q', f.read(8) )[0]
 
 
 # Float
-def ReadFloat(f: BufferedReader, endian: Endian = Endian.LITTLE) -> float: 
-    return unpack( endian + 'f', f.read(4) )[0]
+def ReadFloat(f: BufferedReader, endian = Endian.LITTLE) -> float: 
+    return unpack( str(endian) + 'f', f.read(4) )[0]
 
 #endregion
 
